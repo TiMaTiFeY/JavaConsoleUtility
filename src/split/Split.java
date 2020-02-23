f1d7fd1e47d5b8b83c2cad66397ecade795f7f80
TiMaTiFeY <timatifey.001@yandex.ru>
2020-02-23 14:51:50 +0300

    Added Split for Strings, Chars + File (don't working correct)

--- src/split/Split.java
+++ src/split/Split.java
@@ -1,4 +1,62 @@
 package split;
 
+import java.io.*;
+
 public class Split {
+
+    public enum TypeNum {Strings, Chars, Files}
+
+    private final String ofile;
+    private final boolean flagD;
+    private final TypeNum typeNum;
+    private long num;
+
+    public Split(String ofile, boolean flagD, TypeNum typeNum, long num) {
+        this.ofile = ofile;
+        this.flagD = flagD;
+        this.typeNum = typeNum;
+        this.num = num;
+    }
+
+    private String nextChars(String lastChars) {
+        if (lastChars.charAt(1) == 'z') {
+            return "" + (char) ((int) lastChars.charAt(0) + 1) + 'a';
+        }
+        return "" + lastChars.charAt(0) + (char) ((int) lastChars.charAt(1) + 1);
+    }
+
+    public int splitFile(String inputFileName) throws IOException {
+        try (FileInputStream in = new FileInputStream(inputFileName)) {
+            int countFiles = 0;
+            int lastNum = 1;
+            String lastChars = "aa", outFileName = flagD ? ofile + lastNum : ofile + lastChars;
+            long numReads = num;
+            long numRemainingRead = 0;
+            if (typeNum == TypeNum.Files) {
+                long inputFileSize = new File(inputFileName).length();
+                numReads = inputFileSize / num;
+                numRemainingRead = inputFileSize % num;
+            }
+            try (InputStreamReader reader = new InputStreamReader(in)) {
+                int sym = reader.read();
+                while (sym != -1) {
+                    int i = 0;
+                    try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(outFileName))) {
+                        while (i < numReads + (numRemainingRead > 0 ? 1 : 0) && sym != -1) {
+                            writer.write(sym);
+                            if (typeNum == TypeNum.Chars || typeNum == TypeNum.Files) i++;
+                            else if (sym == '\n' || sym == '\r') i++;
+                            sym = reader.read();
+                        }
+                        if (numRemainingRead > 0) numRemainingRead--;
+                    }
+                    countFiles++;
+                    lastNum++;
+                    if (!flagD) lastChars = nextChars(lastChars);
+                    outFileName = flagD ? ofile + lastNum : ofile + lastChars;
+                }
+            }
+            return countFiles;
+        }
+    }
 }
--- src/split/SplitLauncher.java
+++ src/split/SplitLauncher.java
@@ -7,19 +7,19 @@ import org.kohsuke.args4j.Option;
 
 import java.io.IOException;
 
-public class Split {
+public class SplitLauncher {
 
     @Option(name = "-d", usage = "Set this if output files should have named ofile1, ofile2, ...")
-    private String namingFilesType;
+    private boolean flagD;
 
     @Option(name = "-l", metaVar = "Num", usage = "Set size output files at strings")
-    private int maxLenStrings;
+    private long maxLenStrings;
 
     @Option(name = "-c", metaVar = "Num", usage = "Set size output files at chars", forbids = {"-l"})
-    private int maxLenChars;
+    private long maxLenChars;
 
     @Option(name = "-n", metaVar = "Num", usage = "Set count of output files", forbids = {"-l", "-c"})
-    private int maxCountFiles;
+    private long maxCountFiles;
 
     @Option(name = "-o", metaVar = "Num", usage = "Set default name of output file")
     private String ofile;
@@ -28,9 +28,9 @@ public class Split {
     private String inputFileName;
 
     public static void main(String[] args) {
-        new Split().launch(args);
+        new SplitLauncher().launch(args);
     }
-    
+
     private void launch(String[] args) {
         CmdLineParser parser = new CmdLineParser(this);
 
@@ -38,11 +38,29 @@ public class Split {
             parser.parseArgument(args);
         } catch (CmdLineException e) {
             System.err.println(e.getMessage());
-            System.err.println("java -jar split.jar [-d] [-l num|-c num|-n num] [-o ofile] file");
+            System.err.println("java -jar JavaConsoleUtility.jar [-d] [-l num|-c num|-n num] [-o ofile] file");
             parser.printUsage(System.err);
             return;
         }
 
-    }
+        if (ofile == null) ofile = "x";
+            else if (ofile.equals("-")) ofile = inputFileName;
 
+        Split splitter;
+        if (maxLenStrings != 0)
+            splitter = new Split(ofile, flagD, Split.TypeNum.Strings, maxLenStrings);
+        else if (maxLenChars != 0)
+            splitter = new Split(ofile, flagD, Split.TypeNum.Chars, maxLenChars);
+        else if (maxCountFiles != 0)
+            splitter = new Split(ofile, flagD, Split.TypeNum.Files, maxCountFiles);
+        else
+            splitter = new Split(ofile, flagD, Split.TypeNum.Strings, 100);
+
+        try {
+            int result = splitter.splitFile(inputFileName);
+            System.out.println("Total of " + result + " files created");
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
